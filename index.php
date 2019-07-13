2873a0991b1244f0e7ceed16ba8aae51d8e6f503
ccarter019 <webmaster@coldsnap.net>
2019-07-13 09:18:14 -0700

    first commit initial template files

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require "conn.php"; // Connect to mysql
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="eCommerce shop products for sale">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sales Shop</title>
+
+    <link rel="stylesheet" href="css/custom.css">
+    
+</head>
+<body>
+
+    <header>
+        <h2>eCommerce Shop</h2>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+
+
+<script src="js/custom.js"></script>
+</body>
+</html>
